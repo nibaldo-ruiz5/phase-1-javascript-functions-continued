f38e79c6af04c6d10f04b0d957ed857b76fce0be
Nibaldo Ruiz <nibaldo78@hotmail.com>
2023-04-20 20:26:07 -0400

    with your message here

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+  }
+  
+  const mondayWork = function(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+  }
+  
+  function wrapAdjective(adjective = "*") {
+    return function(param = "special") {
+      return `You are ${adjective}${param}${adjective}!`;
+    }
+  }
+
+  test("my test", () => {
+    // test code
+  }, { timeout: 10000 });
+  
+  
